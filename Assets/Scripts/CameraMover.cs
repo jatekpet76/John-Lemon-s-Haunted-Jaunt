4484ab07b6aa546dbb1710826a975e456f4a8765
Godzzo <egodzzo@gmail.com>
2020-03-01 08:53:00 +0100

    Add TPS view scene - TPSTry and mover script

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+    [SerializeField] float _height = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(_target.position.x, _height, _target.position.z - 4.5f);
+    }
+}
