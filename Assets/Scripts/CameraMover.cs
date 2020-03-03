dbadb3a1d36c62788808db614efcf0746bcde7e6
Godzzo <egodzzo@gmail.com>
2020-03-03 08:38:46 +0100

    Refine TPS Camera and add offsetZ

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -6,6 +6,7 @@ public class CameraMover : MonoBehaviour
 {
     [SerializeField] Transform _target;
     [SerializeField] float _height = 10f;
+    [SerializeField] float _offsetZ = 4.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,6 @@ public class CameraMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(_target.position.x, _height, _target.position.z - 4.5f);
+        transform.position = new Vector3(_target.position.x, _height, _target.position.z - _offsetZ);
     }
 }
